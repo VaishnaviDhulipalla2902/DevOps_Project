31588578273bc7955fad2cc2de799749820e908b
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-05-01 16:55:17 +0530

    Added Dummy Homepage Content!

--- library/views.py
+++ library/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-
+posts = [
+    {
+        'author':'Vaishnavi Dhulipalla',
+        'title' : 'Current Reads',
+        'content': 'I am currently reading The Fountain Head by Ayn Rand.',
+        'date_posted': 'May 1, 2021'
+    },
+        {
+        'author':'Simran Basu',
+        'title' : 'My all-time favourite book!',
+        'content': '**Fill in some info**',
+        'date_posted': 'May 1, 2021'
+    }
+]
 def home(request):
-    return render(request, 'library/home.html')
+    context={
+        'posts' : posts
+    }
+    return render(request, 'library/home.html', context)
 
 
 def about(request):
