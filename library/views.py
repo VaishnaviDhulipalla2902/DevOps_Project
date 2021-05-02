2f4942edddd3f55f60ee124b050b1df990abc705
simran-basu <basusimran18@gmail.com>
2021-05-02 11:29:02 +0530

    Updated html and admins

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
 posts = [
@@ -17,7 +18,7 @@ posts = [
 ]
 def home(request):
     context={
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'library/home.html', context)
 
