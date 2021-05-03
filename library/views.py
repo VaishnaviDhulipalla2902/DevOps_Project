7ad40d8b305084161e126698af64d824b99771a0
simran-basu <basusimran18@gmail.com>
2021-05-03 08:22:47 +0530

    background update

--- library/views.py
+++ library/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
+
 from .models import Post
 # from django.http import HttpResponse
 
-posts = [
-    {
-        'author':'Vaishnavi Dhulipalla',
-        'title' : 'Current Reads',
-        'content': 'I am currently reading The Fountain Head by Ayn Rand.',
-        'date_posted': 'May 1, 2021'
-    },
-        {
-        'author':'Simran Basu',
-        'title' : 'My all-time favourite book!',
-        'content': '**Fill in some info**',
-        'date_posted': 'May 1, 2021'
-    }
-]
 def home(request):
     context={
         'posts' : Post.objects.all()
