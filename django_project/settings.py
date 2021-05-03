517dafb376462e7e1a0b17892f292af9a9e1a051
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-05-03 14:20:44 +0530

    User Registration

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'library.apps.LibraryConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
