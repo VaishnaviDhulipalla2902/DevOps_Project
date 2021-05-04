1612b13f2092335ed46b7b706987e284ca4ec613
VaishnaviDhulipalla2902 <nagasri.vaishnavi@iiitb.org>
2021-05-04 18:57:21 +0530

    Book Image

--- library/models.py
+++ library/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 
 class Post(models.Model):
@@ -9,12 +10,14 @@ class Post(models.Model):
     content = models.TextField(max_length=1000, help_text="Enter a SUMMARY for the book!")
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_img = models.ImageField(default='default_book.jpg', upload_to='book_images')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    
 """ 
 class Books(models.Model):
     title = models.CharField(max_length=100)
